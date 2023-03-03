a581d48b6fc28267eca6dab61ce193857d059c85
cldblz <cloudyshogun@gmail.com>
2023-03-03 15:42:27 +0600

    Update Statistics.jsx

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -2,12 +2,12 @@ import PropTypes from 'prop-types';
 
 const Statistics = ({good, neutral, bad, total, positivePercentage}) => {
     return <div>
-        <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Bad: {bad}</p>
-        <p>Total: {total}</p>
-        <p>Positive feedback: {positivePercentage}%</p>
-    </div>
+                <p>Good: {good}</p>
+                <p>Neutral: {neutral}</p>
+                <p>Bad: {bad}</p>
+                <p>Total: {total}</p>
+                <p>Positive feedback: {positivePercentage}%</p>
+            </div>
  }
 
 Statistics.propTypes = {
