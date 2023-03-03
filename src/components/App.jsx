9ad47a7e17030d436feebe62ef0c0163ba21bd29
cldblz <cloudyshogun@gmail.com>
2023-03-03 23:55:13 +0600

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,49 +1,62 @@
-import { Component } from 'react'
-import Section from './Section'
-import FeedbackOptions from './FeedbackOptions'
-import Statistics from './Statistics'
-import Notification from './Notification'
-import { Container } from './App.styled'
+import { Component } from 'react';
+import Section from './Section';
+import FeedbackOptions from './FeedbackOptions';
+import Statistics from './Statistics';
+import Notification from './Notification';
+import { Container } from './App.styled';
 
 class App extends Component {
   state = {
     good: 0,
     neutral: 0,
-    bad: 0
-  }
+    bad: 0,
+  };
 
   handleFeedback = option => {
     this.setState(prevState => ({
-      [option]: prevState[option] + 1
-    }))
-  }
+      [option]: prevState[option] + 1,
+    }));
+  };
 
   countTotalFeedback = () => {
-    const {good, neutral, bad} = this.state
-    return good + neutral + bad
-  }
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
 
   countPositiveFeedbackPercentage = () => {
-    const { good } = this.state
-    const total = this.countTotalFeedback()
-    return Math.round(good/total*100) || 0
-  }
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return Math.round((good / total) * 100) || 0;
+  };
 
   render() {
-    const { good, neutral, bad } = this.state
-    const { handleFeedback } = this
-    const total = this.countTotalFeedback()
-    const positivePercentage = this.countPositiveFeedbackPercentage()
-    const isFeedbackGiven = !!good || !!neutral || !!bad
+    const { good, neutral, bad } = this.state;
+    const { handleFeedback } = this;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+    const isFeedbackGiven = !!good || !!neutral || !!bad;
 
     return (
       <Container>
         <Section title="Please leave your feedback">
-          <FeedbackOptions options={Object.keys(this.state)} onLeaveFeedback={handleFeedback} />
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={handleFeedback}
+          />
         </Section>
         <Section title="Statistics">
-          {isFeedbackGiven ? <Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={positivePercentage} /> : <Notification message="No feedback given"/>}
-          </Section>
+          {isFeedbackGiven ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          ) : (
+            <Notification message="No feedback given" />
+          )}
+        </Section>
       </Container>
     );
   }
--- src/components/FeedbackOptions.jsx
+++ src/components/FeedbackOptions.jsx
@@ -1,21 +1,29 @@
 import PropTypes from 'prop-types';
-import { Button } from './App.styled'
+import { Button } from './App.styled';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-    return <div>
-        {options.map(option =>
-            <Button type="button" onClick={() => onLeaveFeedback(option)} key={option}>{capitalizeFirstLetter(option)}</Button>
-        )}
-        </div>
- }
+  return (
+    <div>
+      {options.map(option => (
+        <Button
+          type="button"
+          onClick={() => onLeaveFeedback(option)}
+          key={option}
+        >
+          {capitalizeFirstLetter(option)}
+        </Button>
+      ))}
+    </div>
+  );
+};
 
 FeedbackOptions.propTypes = {
-    options: PropTypes.arrayOf(PropTypes.string).isRequired,
-    onLeaveFeedback: PropTypes.func.isRequired,
-}
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
 
-export default FeedbackOptions
+export default FeedbackOptions;
 
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
